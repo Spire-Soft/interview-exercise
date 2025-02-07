74a2c2020b91b4b548b7c8d7eb44fac39adc20be
nick <ndexterfreeman@outlook.com>
2025-02-06 20:28:03 -0800

    clean: decorate data models to ignore certain fields when swagger builds the json request object

--- ticketapi/Models/TicketTask.cs
+++ ticketapi/Models/TicketTask.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace Ticketapi.Models;
 
 public class TicketTask 
 {
+    [JsonIgnore]
     public int Id { get; set; }
     public required string Description { get; set; }
     public required int TicketId { get; set; }
--- ticketapi/Models/user.cs
+++ ticketapi/Models/user.cs
@@ -1,9 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace Ticketapi.Models;
 
 public class User
 {
     public required int Id { get; set; }
     public required string Name { get; set; }
+    [JsonIgnore]
     public List<Project> Projects {get; set; } = [];
+    [JsonIgnore]
     public List<Ticket> Tickets { get; set; } = [];
 }
